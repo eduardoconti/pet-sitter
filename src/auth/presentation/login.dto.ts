b85c2fc5f2d828798782686c343b8821726794c8
Eduardo Conti <es.eduardoconti@gmail.com>
2024-01-27 08:11:54 -0300

    docs: documentacao alguns body

--- src/auth/presentation/login.dto.ts
+++ src/auth/presentation/login.dto.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
 export class LoginDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Email obrigatorio' })
   email!: string;
+
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Senha obrigatoria' })
   senha!: string;
 }
--- src/pet-sitter/presentation/pre-cadastro/pre-cadastro-pet-sitter.dto.ts
+++ src/pet-sitter/presentation/pre-cadastro/pre-cadastro-pet-sitter.dto.ts
@@ -1,21 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsISO8601, IsNotEmpty, IsString } from 'class-validator';
 export class PreCadastroPetSitterDto {
+  @ApiProperty()
   @IsString({ message: 'Nome invalido' })
   @IsNotEmpty({ message: 'Nome obrigatorio' })
   nome!: string;
 
+  @ApiProperty()
   @IsString({ message: 'Sobrenome invalido' })
   @IsNotEmpty({ message: 'Sobrenome obrigatorio' })
   sobreNome!: string;
 
+  @ApiProperty()
   @IsISO8601({}, { message: 'Data nascimento invalida' })
   @IsNotEmpty({ message: 'Data nascimento obrigatoria' })
   dataNascimento!: Date;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Email obrigatorio' })
   @IsEmail({}, { message: 'Email invalido' })
   email!: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Senha obrigatoria' })
   @IsString({ message: 'Senha invalida' })
   senha!: string;
